2fb89d94ba6992241d0b8ac4091e51ca1dceeaa8
blockodyssey-kmyoo <kmyoo@blockodyssey.io>
2023-10-18 18:01:08 +0900

    chore: sirato scripts

--- sirato-down.sh
+++ sirato-down.sh
@@ -1,3 +1,2 @@
-docker compose \
-  -f sirato-free/docker-compose/docker-compose.yml \
-  down --volumes
+# clean up sirato and its docker volume
+docker compose -f sirato-free/docker-compose/docker-compose.yml down --volumes
--- sirato-up.sh
+++ sirato-up.sh
@@ -1,14 +1,13 @@
-# besu network rpc endpoint (currently bootnode's rpc connected to host)
-# see https://github.com/web3labs/sirato-free/tree/master/docker-compose#readme
+# spin up a Sirato instance connected to besu-dev
+# https://github.com/web3labs/sirato-free/tree/master/docker-compose#readme
+
+# set sirato env
 case $(uname -s) in
   Linux*) RPC_HOST=172.16.239.1;;
   Darwin*) RPC_HOST=host.docker.internal;;
   *) RPC_HOST=host.docker.internal;;
 esac
-NODE_ENDPOINT=http://${RPC_HOST}:8545
-# sirato port (default 80)
-PORT=8080
+
 # run sirato
-NODE_ENDPOINT=${NODE_ENDPOINT} PORT=${PORT} docker compose \
-  -f sirato-free/docker-compose/docker-compose.yml \
-  up
+NODE_ENDPOINT="http://${RPC_HOST}:8545" PORT=8080 \
+  docker compose -f sirato-free/docker-compose/docker-compose.yml up
